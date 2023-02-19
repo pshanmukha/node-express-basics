48b40b2491f6a3e55ef9a68e9ee49d47464cc538
Shanmukha <55737606+pshanmukha@users.noreply.github.com>
2023-02-20 03:46:38 +0530

    routes, controllers

--- 02-express/app.js
+++ 02-express/app.js
@@ -3,22 +3,13 @@ const app = express();
 const people = require('./routes/people')
 const auth = require('./routes/auth')
 
-//create express static middleware
-//static assets
+
 app.use(express.static("./methods-public"));
 
-//create urlencoded middleware
-//It parses incoming requests with urlencoded payloads
-//and is based on body-parser.
-//allows to choose between parsing the URL-encoded data
-//with the querystring library (when false)
 app.use(express.urlencoded({ extended: false }));
 
-// parse json
 app.use(express.json());
 
-//to use routers
-//dont forgot to keep "/" infront of base url
 app.use('/api/people', people)
 
 app.use('/login', auth)
--- 02-express/routes/people.js
+++ 02-express/routes/people.js
@@ -1,58 +1,25 @@
-const express = require('express')
-const router = express.Router()
-let { people } = require("../data");
+const express = require("express");
+const router = express.Router();
+const {
+  getPeople,
+  createPerson,
+  updatePerson,
+  deletePerson,
+} = require("../controllers/people");
 
+//router.get("/", getPeople);
 
-//remove base url "/api/people" 
-//beacuse we already mentioned base url in app.js
-//replace base url with forward slash "/"
-router.get("/", (req, res) => {
-    res.status(200).json({ success: true, data: people });
-  });
-  
-  router.post("/", (req, res) => {
-    const {name} = req.body
-    if(!name) {
-      return res.status(400).json({success: false, msg: 'Please provide name value'})
-    }
-    return res.status(200).send({success:true, person:name})
-  })
-  
-  //end point can be same until api method is different
-  router.put("/:id", (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    const person = people.find((person) => person.id === Number(id));
-    if (!person) {
-      return res
-        .status(404)
-        .json({ success: false, msg: `No Person with ID ${id} found` });
-    }
-    console.log(people);
-    console.log(req.body);
-    const newPeople = people.map((person) => {
-      if (person.id === Number(id)) {
-        person.name = name;
-      }
-      return person;
-    });
-    console.log(newPeople);
-    res.status(200).json({ success: true, data: newPeople });
-  });
-  
-  //end point can be same until api method is different
-  router.delete("/:id", (req, res) => {
-    const { id } = req.params;
-    const person = people.find((person) => person.id === Number(id));
-  
-    if (!person) {
-      return res
-        .status(404)
-        .json({ success: false, msg: `No Person with ID ${id} found` });
-    }
-  
-    const newPeople = people.filter((person) => person.id !== Number(id));
-    res.status(200).json({ success: true, data: newPeople });
-  });
+//router.post("/", createPerson);
 
-  module.exports = router
+//router.put("/:id", updatePerson);
+
+//router.delete("/:id", deletePerson);
+
+//or
+//we can use chain functions with the same route
+
+router.route('/').get(getPeople).post(createPerson)
+
+router.route('/:id').put(updatePerson).delete(deletePerson)
+
+module.exports = router;
